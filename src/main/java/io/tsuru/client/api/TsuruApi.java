ee63fc9ebf83aadf86e9cd80a52cf78bdb134c74
gfleury <gfleury@gmail.com>
2018-09-01 16:33:52 +0200

    Adding deployment list

--- src/main/java/io/tsuru/client/api/TsuruApi.java
+++ src/main/java/io/tsuru/client/api/TsuruApi.java
@@ -3569,4 +3569,123 @@ public class TsuruApi implements Serializable {
         apiClient.executeAsync(call, localVarReturnType, callback);
         return call;
     }
+
+
+
+    /**
+     * Build call for appDeployList /1.0/deploys?app=commons&limit=10
+     * @param progressListener Progress listener
+     * @param progressRequestListener Progress request listener
+     * @return Call to execute
+     * @throws ApiException If fail to serialize the request body object
+     */
+    public com.squareup.okhttp.Call appDeployListCall(String appName, int limit, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException {
+        Object localVarPostBody = null;
+
+        // create path and map variables
+        String localVarPath = "/1.0/deploys";
+
+        List<Pair> localVarQueryParams = new ArrayList<Pair>();
+
+        if (appName != null)
+            localVarQueryParams.addAll(apiClient.parameterToPairs("", "app", appName));
+
+        localVarQueryParams.addAll(apiClient.parameterToPairs("", "limit", limit));
+
+        Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+
+        Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+        final String[] localVarAccepts = {
+
+        };
+        final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+        if (localVarAccept != null) localVarHeaderParams.put("Accept", localVarAccept);
+
+        final String[] localVarContentTypes = {
+
+        };
+        final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+        localVarHeaderParams.put("Content-Type", localVarContentType);
+
+        if(progressListener != null) {
+            apiClient.getHttpClient().networkInterceptors().add(new com.squareup.okhttp.Interceptor() {
+                @Override
+                public com.squareup.okhttp.Response intercept(com.squareup.okhttp.Interceptor.Chain chain) throws IOException {
+                    com.squareup.okhttp.Response originalResponse = chain.proceed(chain.request());
+                    return originalResponse.newBuilder()
+                            .body(new ProgressResponseBody(originalResponse.body(), progressListener))
+                            .build();
+                }
+            });
+        }
+
+        String[] localVarAuthNames = new String[] {  };
+        return apiClient.buildCall(localVarPath, "GET", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAuthNames, progressRequestListener);
+    }
+
+    @SuppressWarnings("rawtypes")
+    private com.squareup.okhttp.Call appDeployListValidateBeforeCall(String appName, int limit, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException {
+
+        com.squareup.okhttp.Call call = appDeployListCall(appName, limit, progressListener, progressRequestListener);
+        return call;
+
+    }
+
+    /**
+     * Get Application Deployments
+     * The backend endpoint returns the address that the application is published.
+     * @return List&lt;Deployments&gt;
+     * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
+     */
+    public List<Deployments> appDeployList(String appName, int limit) throws ApiException {
+        ApiResponse<List<Deployments>> resp = appDeployListWithHttpInfo(appName, limit);
+        return resp.getData();
+    }
+
+    /**
+     * Get Application Deployments
+     * The backend endpoint returns the address that the application is published.
+     * @return ApiResponse&lt;List&lt;Application&gt;&gt;
+     * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
+     */
+    public ApiResponse<List<Deployments>> appDeployListWithHttpInfo(String appName, int limit) throws ApiException {
+        com.squareup.okhttp.Call call = appDeployListValidateBeforeCall(appName, limit,null, null);
+        Type localVarReturnType = new TypeToken<List<Deployments>>(){}.getType();
+        return apiClient.execute(call, localVarReturnType);
+    }
+
+    /**
+     * Get Application Deployments (asynchronously)
+     * The backend endpoint returns the address that the application is published.
+     * @param callback The callback to be executed when the API call finishes
+     * @return The request call
+     * @throws ApiException If fail to process the API call, e.g. serializing the request body object
+     */
+    public com.squareup.okhttp.Call appDeployListAsync(String appName, int limit, final ApiCallback<List<Deployments>> callback) throws ApiException {
+
+        ProgressResponseBody.ProgressListener progressListener = null;
+        ProgressRequestBody.ProgressRequestListener progressRequestListener = null;
+
+        if (callback != null) {
+            progressListener = new ProgressResponseBody.ProgressListener() {
+                @Override
+                public void update(long bytesRead, long contentLength, boolean done) {
+                    callback.onDownloadProgress(bytesRead, contentLength, done);
+                }
+            };
+
+            progressRequestListener = new ProgressRequestBody.ProgressRequestListener() {
+                @Override
+                public void onRequestProgress(long bytesWritten, long contentLength, boolean done) {
+                    callback.onUploadProgress(bytesWritten, contentLength, done);
+                }
+            };
+        }
+
+        com.squareup.okhttp.Call call = appDeployListValidateBeforeCall(appName, limit, progressListener, progressRequestListener);
+        Type localVarReturnType = new TypeToken<List<Deployments>>(){}.getType();
+        apiClient.executeAsync(call, localVarReturnType, callback);
+        return call;
+    }
 }
